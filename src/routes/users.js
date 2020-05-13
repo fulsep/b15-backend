6a8efd38ca0e38623fb37802dffec5d76a14595e
Fahrul Septiana <fulsep@gmail.com>
2020-05-13 10:32:33 +0700

    add post route for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,5 +2,6 @@ const route = require('express').Router()
 const userController = require('../controllers/users')
 
 route.get('/', userController.getAllUsers)
+route.post('/', userController.createUser)
 
 module.exports = route
