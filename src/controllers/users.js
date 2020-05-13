c28d92525104849dbde362b5ea5cea0baf39ee80
Fahrul Septiana <fulsep@gmail.com>
2020-05-13 10:33:17 +0700

    add getAllUsers, createUser controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,4 @@
+const userModel = require('../models/users')
 const standardResponse = (_data = {}, res, _status) => {
   const data = {
     success: false,
@@ -12,7 +13,47 @@ const standardResponse = (_data = {}, res, _status) => {
 }
 
 module.exports = {
-  getAllUsers: (req, res) => {
-    standardResponse({ message: 'Hello from /users' }, res)
+  getAllUsers: async (req, res) => {
+    const data = await userModel.getAllUsers()
+    return standardResponse({ message: 'Hello from /users', data }, res)
+  },
+  createUser: async (req, res) => {
+    const { email, password } = req.body
+    if (email && password && email !== '' & password !== '') {
+      const isExists = await userModel.getUsersByCondition({ email })
+      if (isExists.length < 1) {
+        const userData = {
+          email,
+          password,
+          verification_code: 123,
+          role_id: 1,
+          status_id: 1
+        }
+        const create = await userModel.createUser(userData)
+        if (create) {
+          const data = {
+            id: create,
+            ...userData
+          }
+          return standardResponse({
+            message: 'User has been created',
+            data
+          }, res)
+        } else {
+          return standardResponse({
+            message: 'Failed to create user'
+          }, res, 400)
+        }
+      } else {
+        return standardResponse({
+          message: 'Email already used',
+          data: { email }
+        }, res, 400)
+      }
+    } else {
+      return standardResponse({
+        message: 'All form must be filled'
+      }, res, 400)
+    }
   }
 }
