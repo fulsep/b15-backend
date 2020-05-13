4e4aa943648f9845373c119ebc4f003ebca33591
Fahrul Septiana <fulsep@gmail.com>
2020-05-13 15:00:10 +0700

    add patch and delete route

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,7 @@ const userController = require('../controllers/users')
 
 route.get('/', userController.getAllUsers)
 route.post('/', userController.createUser)
+route.patch('/:id', userController.updateUser)
+route.delete('/:id', userController.deleteUser)
 
 module.exports = route
