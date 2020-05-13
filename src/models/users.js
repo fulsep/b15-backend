904bf462b6c563d2466b50ca19e9a069d0521320
Fahrul Septiana <fulsep@gmail.com>
2020-05-13 15:02:16 +0700

    add some function on users model

--- src/models/users.js
+++ src/models/users.js
@@ -1,20 +1,35 @@
 const db = require('../utils/db')
+const mysql = require('mysql')
 
 const queryPromise = (sql, data) => {
   return new Promise((resolve, reject) => {
-    db.query(sql, data, (err, results, fields) => {
+    const a = db.query(sql, data, (err, results, fields) => {
       if (err) {
         reject(Error(err))
       }
       resolve({ results, fields })
     })
+    console.log(a.sql)
   })
 }
 
 module.exports = {
-  getAllUsers: async () => {
-    const sql = 'SELECT * FROM users'
-    const query = await queryPromise(sql)
+  getAllUsers: async (_data = []) => {
+    const order = _data[3] ? mysql.raw('DESC') : mysql.raw('ASC')
+    _data[3] = order
+    const data = _data.length < 6 ? ['email', '%', 'email', order, 5, 0] : _data
+    const sql = `
+    SELECT users.id as id,
+    user_details.picture as picture,
+    user_details.full_name as full_name,
+    user_details.gender as gender,
+    users.email as email,
+    users.password as password,
+    user_details.phone as phone
+    FROM users
+    JOIN user_details ON users.id = user_details.user_id
+    WHERE ? LIKE ? AND users.deleted_at IS NULL ORDER BY ? ? LIMIT ? OFFSET ?`
+    const query = await queryPromise(sql, data)
 
     return query.results
   },
@@ -24,10 +39,43 @@ module.exports = {
 
     return query.results.insertId
   },
+  createUserDetails: async (data) => {
+    const sql = 'INSERT INTO user_details SET ?'
+    const query = await queryPromise(sql, data)
+
+    return query.results.insertId
+  },
   getUsersByCondition: async (data) => {
     const sql = 'SELECT * FROM users WHERE ?'
     const query = await queryPromise(sql, data)
 
     return query.results
+  },
+  getUserDetails: async (data) => {
+    const sql = 'SELECT * FROM user_details WHERE ?'
+    const query = await queryPromise(sql, data)
+
+    return query.results
+  },
+  getUsersCount: async (_data = []) => {
+    const order = _data[3] ? mysql.raw('DESC') : mysql.raw('ASC')
+    _data[3] = order
+    const data = _data.length < 6 ? ['email', '%', 'email', order, 5, 0] : _data
+    const sql = 'SELECT COUNT(users.id) AS total FROM users JOIN user_details ON users.id = user_details.user_id WHERE ? LIKE ? AND users.deleted_at IS NULL ORDER BY ? ?'
+    const query = await queryPromise(sql, data)
+
+    return query.results[0].total
+  },
+  updateUser: async (data) => {
+    const sql = 'UPDATE users SET ? WHERE ?'
+    const query = await queryPromise(sql, data)
+
+    return query.results.affectedRows
+  },
+  updateUserDetails: async (data) => {
+    const sql = 'UPDATE user_details SET ? WHERE ?'
+    const query = await queryPromise(sql, data)
+
+    return query.results.affectedRows
   }
 }
